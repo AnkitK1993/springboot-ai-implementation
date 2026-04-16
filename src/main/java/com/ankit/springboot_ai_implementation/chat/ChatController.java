ca73f18bdae3394d4cb44a3d592f68a1288b9e5e
Ankit Konchady <ankit.konchady@neebal.com>
2026-04-16 23:44:20 +0530

    [CHANGES]--added chatClient

--- src/main/java/com/ankit/springboot_ai_implementation/chat/ChatController.java
+++ src/main/java/com/ankit/springboot_ai_implementation/chat/ChatController.java
@@ -1,5 +1,6 @@
 package com.ankit.springboot_ai_implementation.chat;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatController {
 
     private final OpenAiChatModel openAiChatModel;
+    private final ChatClient chatClient;
 
     @Value("${OPENAI_API_KEY}")
     private String apiKey;
 
-    public ChatController(OpenAiChatModel openAiChatModel){
+    public ChatController(OpenAiChatModel openAiChatModel, ChatClient.Builder builder) {
         this.openAiChatModel = openAiChatModel;
+        this.chatClient = builder.build();
     }
 
     @GetMapping("/test")
@@ -27,9 +30,17 @@ public class ChatController {
         }
     }
 
-    @GetMapping("/chat")
+    @GetMapping("/openai")
     public String getOpenAIChatMessage(@RequestParam("query") String query){
         return openAiChatModel.call(query);
+    }
+
+    @GetMapping("/chat")
+    public String getChatClientMessage(){
+        return chatClient.prompt()
+                .user("Hi")
+                .call()
+                .content();
     }
 
     @GetMapping("/hi")
