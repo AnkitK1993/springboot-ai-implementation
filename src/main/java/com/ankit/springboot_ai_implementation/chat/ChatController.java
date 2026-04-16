48ec4c37ad4532dbf31f07d47a2ad738d53c6683
AnkitK1993 <ankit.konchady@gmail.com>
2026-04-16 23:30:45 +0530

    [CHANGES--controller changes

--- src/main/java/com/ankit/springboot_ai_implementation/chat/ChatController.java
+++ src/main/java/com/ankit/springboot_ai_implementation/chat/ChatController.java
@@ -1,7 +1,7 @@
 package com.ankit.springboot_ai_implementation.chat;
 
-import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,20 +9,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ChatController {
 
-    private final ChatClient chatClient;
     private final OpenAiChatModel openAiChatModel;
 
-    public ChatController(ChatClient.Builder builder, OpenAiChatModel openAiChatModel){
-        this.chatClient = builder.build();
+    @Value("${OPENAI_API_KEY}")
+    private String apiKey;
+
+    public ChatController(OpenAiChatModel openAiChatModel){
         this.openAiChatModel = openAiChatModel;
     }
 
     @GetMapping("/test")
     public String chat(){
-        return chatClient.prompt()
-                .user("Hi")
-                .call()
-                .content();
+        try {
+            return openAiChatModel.call("Who are you");
+        } catch (Exception e) {
+            return "Error: " + e.getMessage();
+        }
     }
 
     @GetMapping("/chat")
@@ -33,6 +35,11 @@ public class ChatController {
     @GetMapping("/hi")
     public String hi(){
         return "Hello";
+    }
+
+    @GetMapping("/apikey")
+    public String getApiKey(){
+        return apiKey;
     }
 
 }
