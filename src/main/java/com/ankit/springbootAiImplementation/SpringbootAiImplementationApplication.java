d20be6ef57096032872bbc3388716773b6574a84
AnkitK1993 <ankit.konchady@gmail.com>
2026-04-17 22:59:08 +0530

    [CHANGES]--added MemoryController

--- src/main/java/com/ankit/springbootAiImplementation/SpringbootAiImplementationApplication.java
+++ src/main/java/com/ankit/springbootAiImplementation/SpringbootAiImplementationApplication.java
@@ -1,10 +1,18 @@
 package com.ankit.springbootAiImplementation;
 
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootAiImplementationApplication {
+
+	@Bean
+	public ChatMemory chatMemory() {
+		return new InMemoryChatMemory();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootAiImplementationApplication.class, args);
